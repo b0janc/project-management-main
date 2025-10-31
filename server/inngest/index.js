7a618676831c57b9ae7e2c659397f56792364e52
b0janc <bojankricc@gmail.com>
2025-10-31 17:37:13 +0700

    Workspace

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -54,6 +54,82 @@ const syncUserUpdation = inngest.createFunction(
 );
 
 
+//inngest function to create workspace from database
+
+const syncWorkspaceCreation = inngest.createFunction(
+  { id: "sync-workspace-from-clerk" },
+  { event: 'clerk/organization.created' },
+  async ({ event, step }) => {
+    const { data } = event
+    await prisma.workspace.create({
+      data: {
+        id: data.id,
+        name: data.name,
+        slug: data.slug,
+        ownerId: data.created_by,
+        image_url: data.image_url,
+      }
+    })
+  }
+);
+
+//Add creator as Admin Member
+
+await prisma.workspaceMember.create({
+  data: {
+    userId: data.created_by,
+    workspaceId: data.id,
+    role: 'ADMIN',
+  }
+})
+
+//inngest function to delete workspace from database
+
+const syncWorkspaceDeletion = inngest.createFunction(
+  { id: "delete-workspace-with-clerk" },
+  { event: 'clerk/organization.deleted' },
+  async ({ event, step }) => {
+    const { data } = event
+    await prisma.workspace.delete({
+      where: { id: data.id },
+    })
+  }
+);
+
+//inngest function to update workspace from database
+
+const syncWorkspaceUpdation = inngest.createFunction(
+  { id: "update-workspace-from-clerk" },
+  { event: 'clerk/organization.updated' },
+  async ({ event, step }) => {
+    const { data } = event
+    await prisma.workspace.update({
+      where: { id: data.id },
+      data: {
+        name: data.name,
+        slug: data.slug,
+        image_url: data.image_url,
+      }
+    })
+  }
+);
+
+//inngest function to save workspace from database
+const syncWorkspaceMemberCreation = inngest.createFunction(
+  { id: "sync-workspace-member-with-clerk" },
+  { event: 'clerk/organizationInvitation.accepted' },
+  async ({ event, step }) => {
+    const { data } = event;
+    await prisma.workspaceMember.create({
+      data: {
+        userId: data.user_id,
+        workspaceId: data.organization_id,
+        role: String(data.role_name).toUpperCase
+      }
+    })
+  }
+);
+
 // Create an empty array where we'll export future Inngest functions
 
 
@@ -61,5 +137,9 @@ const syncUserUpdation = inngest.createFunction(
 export const functions = [
     syncUserCreation,
     syncUserDeletion,
-    syncUserUpdation
+    syncUserUpdation,
+    syncWorkspaceCreation,
+    syncWorkspaceDeletion,
+    syncWorkspaceUpdation,
+    syncWorkspaceMemberCreation
 ];
