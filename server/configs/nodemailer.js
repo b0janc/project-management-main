9ab774ae678c3b11e4e96f7a8b088412e72fb433
b0janc <bojankricc@gmail.com>
2025-12-05 12:18:43 +0700

    josss

--- server/configs/nodemailer.js
+++ server/configs/nodemailer.js
@@ -1,24 +1,27 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false, // true for 465, false for other ports
-  auth: {
-    user: "process.env.SMTP_USER",
-    pass: "process.env.SMTP_PASS",
-  },
+    host: 'smtp-relay.brevo.com', // Sesuaikan dengan host Anda (misal: Brevo)
+    port: 587,
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+    }
 });
 
-const sendEmail = async (to, subject, body) => {
-
-const response = await transporter.sendEmail({
-    from: process.env.SENDER_EMAIL,
-    to,
-    subject,
-    html: body, 
-  });
-  return response;
+const sendEmail = async ({ to, subject, body }) => {
+    try {
+        const response = await transporter.sendMail({ // PERBAIKAN DI SINI
+            from: process.env.SENDER_EMAIL,
+            to: to,
+            subject: subject,
+            html: body 
+        });
+        return response;
+    } catch (error) {
+        console.log("Email Error:", error);
+        throw error;
+    }
 }
 
 export default sendEmail;
