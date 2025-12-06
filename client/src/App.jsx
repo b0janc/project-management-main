0d4aef438150ebb486bb182324406d74c9df1250
b0janc <144081417+b0janc@users.noreply.github.com>
2025-12-07 01:53:16 +0700

    laspoldug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,19 +7,30 @@ import Team from "./pages/Team";
 import ProjectDetails from "./pages/ProjectDetails";
 import TaskDetails from "./pages/TaskDetails";
 
+import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/clerk-react";
+
 const App = () => {
     return (
         <>
             <Toaster />
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Dashboard />} />
-                    <Route path="team" element={<Team />} />
-                    <Route path="projects" element={<Projects />} />
-                    <Route path="projectsDetail" element={<ProjectDetails />} />
-                    <Route path="taskDetails" element={<TaskDetails />} />
-                </Route>
-            </Routes>
+
+            {/* 🔒 User harus login dulu */}
+            <SignedIn>
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<Dashboard />} />
+                        <Route path="team" element={<Team />} />
+                        <Route path="projects" element={<Projects />} />
+                        <Route path="projectsDetail" element={<ProjectDetails />} />
+                        <Route path="taskDetails" element={<TaskDetails />} />
+                    </Route>
+                </Routes>
+            </SignedIn>
+
+            {/* 🔓 Kalau belum login → redirect ke login Clerk */}
+            <SignedOut>
+                <RedirectToSignIn />
+            </SignedOut>
         </>
     );
 };
